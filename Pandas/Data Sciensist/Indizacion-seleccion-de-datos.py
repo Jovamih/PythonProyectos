25dc3cb6a1d24e1c1ef039b4277de2dfc6683f92
Jovamih <Jovamih@user.com>
2020-03-18 17:11:54 -0500

    Se modifico el archivo Indizacion-seleccion-datos.py

--- Pandas/Data Sciensist/Indizacion-seleccion-de-datos.py
+++ Pandas/Data Sciensist/Indizacion-seleccion-de-datos.py
@@ -33,5 +33,66 @@ def indizacion2():
     print(data.loc['Chile']) #error poner indices que no existen
     #iloc usa los indices implicitos 0,,1,..
     print(data.iloc[2:4])
+    
+def dataframes():
+    #acceso modo diccionario
+    population_dict={'China':120000000,
+                'Peru':90000,
+                'Chile':89098830,
+                'Colombia':120000,
+                'Argentina':1299000
+                }
+    infected_dict={
+                'China':890000,
+                'Peru':900,
+                'Chile':89000,
+                'Colombia':52009,
+                'Argentina':1230
+            }
+    population=pd.Series(population_dict)
+    infected=pd.Series(infected_dict)
+    data=pd.DataFrame({'Poblacion':population,'Infectados':infected})
+    print(data)
+    print(data['Poblacion'])
+    print(data.Poblacion) #se dee evitar usar este tipo de acceso por seguridad
+    #tambien se pueen crear otras columnas
+    data['Porcentaje']=(data['Infectados']/data['Poblacion'])*100
+    print(data)
+    #Transposicion de columnas
+    print(data.T)
+    
+def dataframe_index():
+    #accesos a los datframes
+    #acceso por enmascaramiento
+    population_dict={'China':120000000,
+                'Peru':90000,
+                'Chile':89098830,
+                'Colombia':120000,
+                'Argentina':1299000
+                }
+    infected_dict={
+                'China':890000,
+                'Peru':900,
+                'Chile':89000,
+                'Colombia':52009,
+                'Argentina':1230
+            }
+    population=pd.Series(population_dict)
+    infected=pd.Series(infected_dict)
+    data=pd.DataFrame({'Poblacion':population,'Infectados':infected})
+    data['Porcentaje']=(data['Infectados']/data['Poblacion'])*100
+    #tenemos indeacion implicita a traves de indice snetros e explicita (nombre verdaderos)
+    print(data.iloc[4,:])
+    #explicita
+    print(data.loc['Chile':'Argentina',:])
+    print(data.loc[data['Porcentaje']>30]) # se puede poner sin loc, si solo se hace referencia a la fila data[data.something>do]
+    print(data.loc[data.Porcentaje>10,['Poblacion']])
+    #posee propiedaes similares a las matricez Numpy
+    #slicing
+    print(data.loc[:,'Poblacion':'Infectados'])
+    print(data['Colombia':'Argentina']) #solo podemos acceder a las filas
+    #con data['Colombia']: accedemos a las columnas
+    #Lod tipos de acceso por enmascaramiento y por indexacion eleante se pueden  combinar
+    
 if __name__=="__main__":
-    indizacion2()
+    dataframe_index()
