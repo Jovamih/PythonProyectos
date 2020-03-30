ffd3d979635057cbaa73eafc43d7bc02cb30e0da
Jovamih <Jovamih@user.com>
2020-03-30 14:45:33 -0500

    Cambios no importantes

--- Matplotlib/customize-colorbar.py
+++ Matplotlib/customize-colorbar.py
@@ -6,13 +6,15 @@ import matplotlib.pyplot as plt
 
 def Graphic():
     #si queremos personalizar la barra de colores que se muestra podemos usar colorbar() y sus propiedades
-    x=np.linspace(1,10,1000)
+    x=np.linspace(0,10,1000)
     I=np.sin(x)*np.cos(x[:,np.newaxis])
     #usamo imshow() cuando queremos mostrar grafico de matrices multiples
-    plt.imshow(I)
+    plt.imshow(I,cmap='RdGy')
     plt.colorbar(label='Grafico virtual')
-
     plt.show()
+    
+def Graphic2():
+    pass
 
 if __name__=="__main__":
     Graphic()
