7ef9a7159485dd069a23cfaffe83eed0c8a76b9d
Jovamih <Jovamih@user.com>
2020-04-13 18:35:26 -0500

    Html_to_csv agregado como conversor de tablas html en csv

---
+++ Pytest/html_To_Csv.py
@@ -0,0 +1,20 @@
+# /usr/bin/env python3
+
+#1. Este script convierte una tabla encontrada en fichero html a  formato CSV
+#paa ello realizara los siguientes pasos
+
+#1. Funcion que detecte el inicio y final  de una tabla  en codigo html, para que mas rendimiento en vez de
+    #iterar linea por linea buscando ducho patron de busqueda
+#2. Construir una funcio que encuentre determinada palabra entre un conjunto de caracteres especificadas
+    #ej. buscar entre 'a' y 'mn'-> 'la determinada tranr tmn': devuelve: 'nr t'
+#3 funcion que identifique las columna sde una tabla html
+#4 funcion que identifique las filas de una tabla html
+#5 funcion principal que integre las demas funciones y escribba en fichero csv
+    
+ 
+def main():
+    #code Here!
+    pass
+
+if __name__=="__main__":
+    main()
