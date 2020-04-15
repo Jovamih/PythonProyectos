0e4f83e9e92214fdb0a10ebda5abe72473e932d6
Jovamih <avira365@hotmail.com>
2020-04-15 17:31:08 -0500

    Cambios importantes en tocsv.py

--- Pytest/tocsv.py
+++ Pytest/tocsv.py
@@ -33,7 +33,6 @@ def search_advanced_word(word=str(),start=str(),end=str()):
             if last!=-1:
                 currency.append(word[:last])
                 word=word[last+len(end):]
-                print("1era y2da coincidencia encontrada")
 
             else: break
         else :
@@ -44,22 +43,22 @@ def search_advanced_modified(word=str(),start=str(),end=str()):
     
     lista=search_advanced_word(word,start,end)
     #print(lista)
-    return [body[body.find(">")+1:] for body in lista]
+    return [body[body.find(">")+1 :] for body in lista]
     
 
 
 def main(path=str()):
-    os.chdir(os.path.dirname(path))
+    #os.chdir(os.path.dirname(path))
     filename=os.path.basename(path)
     filename=filename[:filename.rfind(os.path.extsep)]+"{0}.csv"
     with open(path,'r') as f:
         data=f.readlines()
-    data=[line.strip() for line in data]
+    data=[line.strip() for line in data if line]
     texto="".join(data)
     #print(texto)
     #primeo buscamos las tablas
     tables=search_advanced_modified(texto,start='<table',end='</table>')
-    print("{0} tablas".format(len(tables)))
+    print("{0} Tablas encontradas".format(len(tables)))
     for i,table in enumerate(tables):
         #creamos una lista de datos de cada fila
         datarows=list()
@@ -68,11 +67,9 @@ def main(path=str()):
         #asi que primero obtenemos la cabezera y de ahi obtendremos el nombre de las columnas
         thead=search_advanced_word(table,start='<thead>',end='</thead>') #lista de cabezeras
         # como solo tiene una, obtenemos el primer elemento y obtenms las columnas
-        print("Thead {}".format(thead[0]))
         columns=search_advanced_modified(thead[0],start='<th',end='</th>')
-        print("Columnas".center(10,"-"))
-        print(columns)
-        datarows.append(columns)
+        print("\tT{0}: {1} Columnas encontradas".format(i+1,len(columns)))
+        #datarows.append(columns)
         #ahora obtenemos el cuerpo
         
         tbody=search_advanced_word(table,start='<tbody>',end='</tbody>')
@@ -81,28 +78,28 @@ def main(path=str()):
         rows=search_advanced_modified(tbody[0],start='<tr',end='</tr>')
         #ahora una vez obtenido la lista de filas obtendremos los elemento que corresponden
         #a cada columna
-        print("{} fila encontradas".format(len(rows)))
+        print("\tT{0}: {1} Filas encontradas".format(i+1,len(rows)))
         for row in rows:
             #obtenemos los datos de cada fila
-            data=search_advanced_word(row,start='<td>',end='</td>')
+            data=search_advanced_modified(row,start='<td>',end='</td>')
             #y luego los agregamos
             datarows.append(data)
 
-        with open(filename.format(i),"w") as f:
-            writer=csv.writer(f)
-            writer.writerows(rows)
-        print('Tabla {0} creada correctamente'.format(i+1))
+        csvdata=pd.DataFrame(datarows,columns=columns)
+        csvdata.to_csv(filename.format(i))
+        print('\tTabla {0} creada correctamente como {1}'.format(i+1,filename.format(i)))
         
 if __name__=="__main__":
     import sys
     import os
+    import pandas as pd
     
-    """if len(sys.argv)<2:
+    if len(sys.argv)<2:
         print('<path> Parameter not found')
         sys.exit(0)
     
     if not os.path.isfile(sys.argv[1]):
         print("La ruta especificada hace alusion a un archivo que no existe")
-        sys.exit(0)"""
-    main(r"d:\fits\data.html")
+        sys.exit(0)
+    main(sys.argv[1])
     
